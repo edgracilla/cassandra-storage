0de2876fe250e56e3d94941b686d8ee114560f78
Benj Sicam <bsicam@reekoh.com>
2016-04-30 14:31:04 +0800

    Refactor Code

--- app.js
+++ app.js
@@ -2,140 +2,173 @@
 
 var platform      = require('./platform'),
 	async         = require('async'),
-	isPlainObject = require('lodash.isplainobject'),
+	isNil         = require('lodash.isnil'),
 	moment        = require('moment'),
-	parseFields, client, tableName;
+	isEmpty       = require('lodash.isempty'),
+	isArray       = require('lodash.isarray'),
+	isNumber      = require('lodash.isnumber'),
+	isString      = require('lodash.isstring'),
+	isBoolean     = require('lodash.isboolean'),
+	isPlainObject = require('lodash.isplainobject'),
+	fieldMapping, client, tableName;
 
-/*
- * Listen for the data event.
- */
-platform.on('data', function (data) {
-	var columnList,
-		valueList,
-		valueRef = {},
-		first    = true;
+let insertData = function (data, callback) {
+	let query = `insert into ${tableName} (${data.columns.join(', ')}) values (${data.values.join(', ')})`;
 
-	async.forEachOf(parseFields, (field, key, callback) => {
-		var datum = data[field.source_field],
+	client.execute(query, data.data, {prepare: true}, (insertError) => {
+		callback(insertError);
+	});
+};
+
+let processData = function (data, callback) {
+	let keyCount      = 0,
+		processedData = {
+			columns: [],
+			values: [],
+			data: {}
+		};
+
+	async.forEachOf(fieldMapping, (field, key, done) => {
+		keyCount++;
+
+		processedData.columns.push(`"${key}"`);
+		processedData.values.push(`:val${keyCount}`);
+
+		let datum = data[field.source_field],
 			processedDatum;
 
-		if (datum !== undefined && datum !== null) {
-			if (field.data_type) {
-				try {
-					if (field.data_type === 'String') {
-						if (isPlainObject(datum))
-							processedDatum = JSON.stringify(datum);
-						else
-							processedDatum = String(datum);
-
-					} else if (field.data_type === 'Integer') {
-
-						var intData = parseInt(datum);
+		if (!isNil(datum) && !isEmpty(field.data_type)) {
+			try {
+				if (field.data_type === 'String') {
+					if (isPlainObject(datum))
+						processedDatum = JSON.stringify(datum);
+					else
+						processedDatum = `${datum}`;
+				}
+				else if (field.data_type === 'Integer') {
+					if (isNumber(datum))
+						processedDatum = datum;
+					else {
+						let intData = parseInt(datum);
 
 						if (isNaN(intData))
 							processedDatum = datum; //store original value
 						else
 							processedDatum = intData;
-
-					} else if (field.data_type === 'Float') {
-
-						var floatData = parseFloat(datum);
+					}
+				}
+				else if (field.data_type === 'Float') {
+					if (isNumber(datum))
+						processedDatum = datum;
+					else {
+						let floatData = parseFloat(datum);
 
 						if (isNaN(floatData))
 							processedDatum = datum; //store original value
 						else
 							processedDatum = floatData;
-
-					} else if (field.data_type === 'Boolean') {
-
-						var type = typeof datum;
-
-						if ((type === 'string' && datum.toLocaleLowerCase() === 'true') ||
-							(type === 'number' && datum === 1 )) {
-							processedDatum = true;
-						} else if ((type === 'string' && datum.toLocaleLowerCase() === 'false') ||
-							(type === 'number' && datum === 0 )) {
-							processedDatum = false;
-						} else {
-							processedDatum = datum;
-						}
-					} else if (field.data_type === 'DateTime') {
-
-						var dtm = new Date(datum);
-						if (!isNaN(dtm.getTime())) {
-
-							if (field.format !== undefined)
-								processedDatum = moment(dtm).format(field.format);
-							else
-								processedDatum = dtm;
-
-
-						} else {
-							processedDatum = datum;
-						}
-					} else if (field.data_type === 'JSON') {
-						try {
-							JSON.parse(datum);
-							if (isPlainObject(datum))
-								processedDatum = datum;
-							else
-								processedDatum = JSON.parse(datum);
-
-							console.log('in try');
-						}
-						catch (e) {
-							processedDatum = datum;
-						}
 					}
-				} catch (e) {
-					if (typeof datum === 'number')
+				}
+				else if (field.data_type === 'Boolean') {
+					if (isBoolean(datum))
 						processedDatum = datum;
-					else if (isPlainObject(datum))
-						processedDatum = JSON.stringify(datum);
+					else {
+						if ((isString(datum) && datum.toLowerCase() === 'true') || (isNumber(datum) && datum === 1))
+							processedDatum = true;
+						else if ((isString(datum) && datum.toLowerCase() === 'false') || (isNumber(datum) && datum === 0))
+							processedDatum = false;
+						else
+							processedDatum = (datum) ? true : false;
+					}
+				}
+				else if (field.data_type === 'Timestamp') {
+					if (isEmpty(field.format) && moment(datum).isValid())
+						processedDatum = moment(datum).toDate();
+					else if (!isEmpty(field.format) && moment(datum, field.format).isValid())
+						processedDatum = moment(datum, field.format).toDate();
+					else if (!isEmpty(field.format) && moment(datum).isValid())
+						processedDatum = moment(datum).toDate();
 					else
 						processedDatum = datum;
+				} else if (field.data_type === 'Map') {
+					try {
+						if (isPlainObject(datum))
+							processedDatum = datum;
+						else
+							processedDatum = JSON.parse(datum);
+					}
+					catch (e) {
+						processedDatum = datum;
+					}
+				} else if (field.data_type === 'Set') {
+					try {
+						if (isArray(datum))
+							processedDatum = datum;
+						else
+							processedDatum = JSON.parse(datum);
+					}
+					catch (e) {
+						processedDatum = datum;
+					}
 				}
-
-			} else {
-				if (typeof datum === 'number')
-					processedDatum = datum;
-				else if (isPlainObject(datum))
+			}
+			catch (e) {
+				if (isPlainObject(datum))
 					processedDatum = JSON.stringify(datum);
 				else
 					processedDatum = datum;
 			}
-
-		} else {
+		}
+		else if (!isNil(datum) && isEmpty(field.data_type)) {
+			if (isPlainObject(datum))
+				processedDatum = JSON.stringify(datum);
+			else
+				processedDatum = `${datum}`;
+		}
+		else
 			processedDatum = null;
-		}
 
-		valueRef[key] = processedDatum;
+		processedData.data[`val${keyCount}`] = processedDatum;
 
-		if (!first) {
-			valueList = valueList + ',:' + key;
-			columnList = columnList + ',' + key;
-		} else {
-			first = false;
-			valueList = ':' + key;
-			columnList = key;
-		}
-
-		callback();
-
+		done();
 	}, () => {
-		client.execute('insert into ' + tableName + ' (' + columnList + ') values (' + valueList + ')', valueRef, {prepare: true}, function (reqErr, queryset) {
-			if (reqErr) {
-				console.error('Error creating record on Cassandra', reqErr);
-				platform.handleException(reqErr);
-			} else {
-				platform.log(JSON.stringify({
-					title: 'Record Successfully inserted to Cassandra.',
-					data: valueRef
-				}));
-			}
-		});
+		callback(null, processedData);
 	});
+};
 
+platform.on('data', function (data) {
+	if (isPlainObject(data)) {
+		processData(data, (error, processedData) => {
+			insertData(processedData, (error) => {
+				if (!error) {
+					platform.log(JSON.stringify({
+						title: 'Record Successfully inserted to Cassandra Database.',
+						data: data
+					}));
+				}
+				else
+					platform.handleException(error);
+			});
+		});
+	}
+	else if (isArray(data)) {
+		async.each(data, function (datum) {
+			processData(datum, (error, processedData) => {
+				insertData(processedData, (error) => {
+					if (!error) {
+						platform.log(JSON.stringify({
+							title: 'Record Successfully inserted to Cassandra Database.',
+							data: datum
+						}));
+					}
+					else
+						platform.handleException(error);
+				});
+			});
+		});
+	}
+	else
+		platform.handleException(new Error(`Invalid data received. Data must be a valid Array/JSON Object or a collection of objects. Data: ${data}`));
 });
 
 /*
@@ -164,38 +197,44 @@ platform.once('ready', function (options) {
 	var isEmpty   = require('lodash.isempty'),
 		cassandra = require('cassandra-driver');
 
+	tableName = options.table;
+
 	async.waterfall([
-		async.constant(options.fields || {}),
-		async.asyncify(JSON.parse)
-	], (parseError, parseFields) => {
+		async.constant(options.field_mapping || '{}'),
+		async.asyncify(JSON.parse),
+		(obj, done) => {
+			fieldMapping = obj;
+			done();
+		}
+	], (parseError) => {
 		if (parseError) {
-			platform.handleException(new Error('Invalid option parameter: fields. Must be a valid JSON String.'));
+			platform.handleException(new Error('Invalid field mapping. Must be a valid JSON String.'));
 
 			return setTimeout(() => {
 				process.exit(1);
-			}, 2000);
+			}, 5000);
 		}
 
-		async.forEachOf(parseFields, (field, key, callback) => {
+		async.forEachOf(fieldMapping, (field, key, done) => {
 			if (isEmpty(field.source_field))
-				callback(new Error('Source field is missing for ' + key + ' in field mapping.'));
+				done(new Error(`Source field is missing for ${key} in field mapping.`));
 			else if (field.data_type && (field.data_type !== 'String' && field.data_type !== 'Integer' &&
 				field.data_type !== 'Float' && field.data_type !== 'Boolean' &&
-				field.data_type !== 'DateTime' && field.data_type !== 'JSON')) {
+				field.data_type !== 'Timestamp' && field.data_type !== 'Map' && field.data_type !== 'Set')) {
 
-				callback(new Error('Invalid Data Type for ' + key + ' allowed data types are (String, Integer, Float, Boolean, DateTime, JSON).'));
+				done(new Error(`Invalid Data Type for ${key}. Allowed data types are String, Integer, Float, Boolean, Timestamp, Map and Set.`));
 			}
 			else
-				callback();
+				done();
 
-		}, (error) => {
-			if (error) {
-				console.error('Error parsing JSON field mapping.', error);
-				platform.handleException(error);
+		}, (fieldMapError) => {
+			if (fieldMapError) {
+				console.error('Error parsing JSON field mapping.', fieldMapError);
+				platform.handleException(fieldMapError);
 
 				return setTimeout(() => {
 					process.exit(1);
-				}, 2000);
+				}, 5000);
 			}
 
 			var host = `${options.host}`.replace(/\s/g, '').split(',');
@@ -209,16 +248,14 @@ platform.once('ready', function (options) {
 
 			if (options.port) client.protocolOptions = {port: options.port};
 
-			tableName = options.table;
-
 			client.connect((connectionError) => {
 				if (connectionError) {
 					console.error('Error connecting in Cassandra.', connectionError);
 					platform.handleException(connectionError);
 
-					return setTimeout(() => {
+					setTimeout(() => {
 						process.exit(1);
-					}, 2000);
+					}, 5000);
 				} else {
 					platform.log('Cassandra Storage plugin ready.');
 					platform.notifyReady();
